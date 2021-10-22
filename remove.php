aa5af1e3a9142adb5465d8f6895edc574c679e88
stephencarrano <91911995+stephencarrano@users.noreply.github.com>
2021-10-22 12:31:31 -0400

    Create remove.php

---
+++ remove.php
@@ -0,0 +1,61 @@
+<html>
+
+<body>
+
+
+
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
+$dbname = "project";
+
+
+
+$phone= $_POST["phonenumber"];
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+
+
+
+if (!$conn) {
+
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+
+
+$sql = "DELETE FROM WAITLIST WHERE (Phone_Number = '$phone')";
+
+
+
+//$result = $conn->query($sql);
+
+if ($conn->query($sql) === TRUE) {
+
+    echo "Sign up successfully!";
+
+    header("location: index.html");
+
+} 
+
+else {
+
+    echo "Error: " . $sql . "<br>" . $conn->error;
+
+}
+
+
+
+$conn->close();
+
+
+?>
+
+</body>
+
+</html>
