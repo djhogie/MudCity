e07ce4daf62bfc21a8eb6834ece1865cb21ea6a6
Delaney Brennan <brenn104@go.stockton.edu>
2021-11-19 15:30:47 -0500

    fixed sms

--- sms.php
+++ sms.php
@@ -16,16 +16,23 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$pNum = $_POST["pNum"];
+$pNum = $_POST["phoneNumber"];
 
 $message = "Thank you for coming to Mud City Crab House. Your table will be ready in 15 minutes!";
  
   $mail->Username = "mudcitytexts@gmail.com"; // your GMail user name
     $mail->Password = "mudCity1185"; 
     
-    $mail->AddAddress( "6093398456@tmomail.net" ); // recipients email
-     $mail->AddAddress( "6093398456@vtext.com" ); // recipients email
-      $mail->AddAddress( "6093398456@txt.att.net" ); // recipients email
+    
+     $mail->AddAddress( $pNum . "@vtext.com" ); // recipients email
+      $mail->AddAddress( $pNum . "@vzwpix.com" ); // recipients email
+      $mail->AddAddress( $pNum . "@messaging.sprintpcs.com" ); // recipients email
+      $mail->AddAddress( $pNum . "@txt.att.net" ); // recipients email
+      $mail->AddAddress( $pNum . "@tmomail.net" ); // recipients email
+      $mail->AddAddress( $pNum . "@vmobl.com" ); // recipients email
+      $mail->AddAddress( $pNum . "@mmst5.tracfone.com" ); // recipients email
+      $mail->AddAddress( $pNum . "@email.uscc.net" ); // recipients email
+      $mail->AddAddress( $pNum . "@mymetropcs.com" ); // recipients email
       
     $mail->FromName = "Mud City Crab House"; // readable name
 
@@ -36,8 +43,10 @@ $message = "Thank you for coming to Mud City Crab House. Your table will be read
     $mail->IsSMTP();     // use SMTP
     $mail->SMTPAuth = true; // turn on SMTP authentication
     $mail->From = $mail->Username;
-    if(!$mail->Send())
+  if(!$mail->Send())
         echo "Mailer Error: " . $mail->ErrorInfo;
+        
     else
         echo "Message has been sent";
+        header("location: index.html");
 ?>
